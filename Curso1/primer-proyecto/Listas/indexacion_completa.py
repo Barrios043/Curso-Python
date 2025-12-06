55b72c8b103cdd985b55147beb7e8f4e2b4af7fc
Barrios043 <lbw2812@gmail.com>
2025-12-05 22:13:58 -0400

    Indexacion de Lista

---
+++ Curso1/primer-proyecto/Listas/indexacion_completa.py
@@ -0,0 +1,16 @@
+nombres = ["Maria", "Pedro", "Cipriano", "Roberto"]
+print(nombres)
+
+print(nombres[0]) #Output : Maria
+print(nombres[1]) #Output : Pedro
+print(nombres[2]) #Output : Cipriano
+print(nombres[3]) #Output : Roberto
+
+print(nombres[-1]) #Output : Roberto
+print(nombres[-2]) #Output : Cipriano
+print(nombres[-3]) #Output : Pedro
+print(nombres[-4]) #Output : Maria
+
+print(nombres[1:4]) #Output : ['Pedro', 'Cipriano', 'Roberto']
+print(nombres[:2]) #Output :  ['Maria', 'Pedro']
+print(nombres[2:]) #Output : ['Cipriano', 'Roberto']
